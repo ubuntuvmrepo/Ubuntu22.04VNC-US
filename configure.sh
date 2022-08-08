846cf1ce55c9e5b0e4d090f9dcc97797442954b1
Growtopia Jaw <growtopiajaw@gmail.com>
2022-08-08 11:34:36 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,10 +1,8 @@
 #configure.sh VNC_PASSWORD NGROK_AUTH_TOKEN
 
 #Enable VNC
-wget "http://tigervnc.bphinz.com/nightly/ubuntu-20.04LTS/amd64/tigervncserver_1.12.80+20220801git07d4e290-1ubuntu1_amd64.deb" -O tigervncserver.deb
 sudo apt update
-sudo apt install ./tigervncserver.deb -y
-sudo apt --fix-broken install -y
+sudo apt install lxqt tightvncserver -y
 mkdir -p $HOME/.vnc
 vncpasswd -f <<< $1 > "$HOME/.vnc/passwd"
 
