ad177722c2528efd9cd70a5f2ddaba74b52f5f92
Growtopia Jaw <growtopiajaw@gmail.com>
2022-08-13 20:58:52 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,4 +1,6 @@
-#configure.sh VNC_PASSWORD NGROK_AUTH_TOKEN
+#configure.sh VNC_PASSWORD NGROK_AUTH_TOKEN DDNS CLOUDFLARED_CERT CLOUDFLARED_CONFIG_LINUX_SSH CLOUDFLARED_KEY_LINUX_SSH CLOUDFLARED_NAME_LINUX_SSH CLOUDFLARED_CONFIG_LINUX_VNC CLOUDFLARED_KEY_LINUX_VNC CLOUDFLARED_NAME_LINUX_VNC
+#             1            2                3    4                5                            6                         7                          8                            9                         10
+CLOUDFLARED_VERSION=2022.8.0
 
 #Enable VNC
 sudo apt update
@@ -11,12 +13,25 @@ vncpasswd -f <<<"$1" >"$HOME/.vnc/passwd"
 #Start VNC/reset changes
 vncserver
 
-#install ngrok
-curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null
-echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list
-sudo apt update
-sudo apt install ngrok -y
-
-#configure ngrok and start it
-ngrok authtoken $2
-ngrok tcp 5901 &
+if [ "$3" == "ngrok" ]; then
+  #install & start ngrok
+  curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null
+  echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list
+  sudo apt update
+  sudo apt install ngrok -y
+  ngrok authtoken $2
+  ngrok tcp 5901 &
+else
+  #install & start cloudflared
+  wget https://github.com/cloudflare/cloudflared/releases/download/"$CLOUDFLARED_VERSION"/cloudflared-linux-amd64
+  sudo install cloudflared-linux-amd64 /usr/bin/cloudflared
+  rm cloudflared-linux-amd64
+  mkdir -p "$HOME/.cloudflared"
+  echo "${4}" > "$HOME/.cloudflared/cert.pem"
+  echo "${5}" > "$HOME/.cloudflared/${7}.yml"
+  echo "${6}" > "$HOME/.cloudflared/${7}.json"
+  cloudflared --config "$HOME/.cloudflared/${7}.yml" tunnel run "${7}" &
+  echo "${8}" > "$HOME/.cloudflared/${10}.yml"
+  echo "${9}" > "$HOME/.cloudflared/${10}.json"
+  cloudflared --config "$HOME/.cloudflared/${10}.yml" tunnel run "${10}" &
+fi
